3f72b09adc20ea939c61bec5b107912d49fae675
brightboost <maaikejvp@hotmail.com>
2024-01-24 17:59:37 +0100

    style

--- 4 - Modifying DOM elements/04_05/script.js
+++ 4 - Modifying DOM elements/04_05/script.js
@@ -1,4 +1,18 @@
 function adjustStyle() {
     let textToStyle = document.getElementById('textToStyle');
 
+    // Changing background color
+    textToStyle.style.backgroundColor = 'yellow';
+
+    // Changing font size
+    textToStyle.style.fontSize = '20px';
+
+    // Changing text color
+    textToStyle.style.color = 'blue';
+
+    // Adding padding
+    textToStyle.style.padding = '10px';
+
+    // Changing font weight
+    textToStyle.style.fontWeight = 'bold';
 }
