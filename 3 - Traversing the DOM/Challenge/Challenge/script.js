7bda87019ed0806f77d532a46f79645a7f17704f
brightboost <maaikejvp@hotmail.com>
2024-01-24 16:42:33 +0100

    challenge

---
+++ 3 - Traversing the DOM/Challenge/Challenge/script.js
@@ -0,0 +1,6 @@
+// Your tasks:
+// 1. Find the parent element of a cat and log it to the console.
+
+// 2. List all the children of the cat’s parent and log them.
+
+// 3. Identify the cat’s parent's next sibling in the tree and log it.
