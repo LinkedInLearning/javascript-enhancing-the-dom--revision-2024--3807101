14a6f5663ded86e2bf4cce64b2cd34339ac570c3
brightboost <maaikejvp@hotmail.com>
2024-01-25 13:36:56 +0100

    start

---
+++ 8 - Accessibility, performance and best practices/08_01/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const employeeList = document.getElementById('employeeList');
+    const employees = ['John Doe', 'Jane Smith', 'Emma Johnson'];
+
+    
+});
