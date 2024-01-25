547a46a9ed53f7ffb85d65d2824d4681274b3679
brightboost <maaikejvp@hotmail.com>
2024-01-25 10:32:53 +0100

    start

---
+++ 7 - Advanced DOM manipulation/07_02/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const todoList = document.getElementById('todoList');
+    const newTaskInput = document.getElementById('newTaskInput');
+    const addTaskButton = document.getElementById('addTaskButton');
+    const removeFirstTaskButton = document.getElementById('removeFirstTaskButton');
+    const prioritizeLastTaskButton = document.getElementById('prioritizeLastTaskButton');
+});
