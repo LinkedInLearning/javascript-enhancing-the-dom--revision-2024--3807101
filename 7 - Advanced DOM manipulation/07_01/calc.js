42e938c5f12110f1c73f2884ba7c7b193460c354
brightboost <maaikejvp@hotmail.com>
2024-01-25 10:32:04 +0100

    end

--- 7 - Advanced DOM manipulation/07_01/calc.js
+++ 7 - Advanced DOM manipulation/07_01/calc.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-   
+    const calculateButton = document.getElementById('calculateButton');
+    const calculationResult = document.getElementById('calculationResult');
+
+    calculateButton.addEventListener('click', () => {
+        let number1 = document.getElementById('number1').value;
+        let number2 = document.getElementById('number2').value;
+
+        let result = parseInt(number1) + parseInt(number2);
+        calculationResult.textContent = `Result: ${isNaN(result) ? 'Invalid input' : result}`;
+    });
 });
