718fe0a351dbafce714afe92be5ef72dc19f2e6b
brightboost <maaikejvp@hotmail.com>
2024-01-26 23:14:57 +0100

    show sections

--- 9 - Interactive resume project/public/js/main.js
+++ 9 - Interactive resume project/public/js/main.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    showSection('experiences');
 });
 
 function showSection(sectionId) {
-    
+    // Hide all sections
+    document.getElementById('experiences').classList.remove('active');
+    document.getElementById('contact').classList.remove('active');
+
+    // Show the one that is selected
+    document.getElementById(sectionId).classList.add('active');
+
+    // Update navbar links to reflect active session
+    const navbarLinks = document.querySelectorAll('#navbar a');
+    navbarLinks.forEach(link => {
+        link.classList.remove('active');
+        if(link.textContent.toLowerCase() === sectionId) {
+            link.classList.add('active');
+        }
+    })
 }
