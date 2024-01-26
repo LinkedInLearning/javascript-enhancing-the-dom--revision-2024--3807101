29ae0af04aacd4d6d69c4db9a6f5863f8507df5a
brightboost <maaikejvp@hotmail.com>
2024-01-26 22:30:25 +0100

    update

--- 9 - Interactive resume project/server/server.js
+++ 9 - Interactive resume project/server/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+app.use('/api/contact', contactsRouter);
 app.use('/api/experiences', experiencesRouter);
 app.use('/api/skills', skillsRouter);
 
