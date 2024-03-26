bb08350ec7e7cd22bcf75325c75957bad9e8029a
Maaike <maaikejvp@hotmail.com>
2024-03-26 19:43:54 +0100

    starter lines

---
+++ 3 - Traversing the DOM/03_03/script.js
@@ -0,0 +1,4 @@
+// Using 'closest'
+let newsContent = document.getElementById('newsContent');
+let closestArticle = newsContent.closest('.article');
+console.log('Closest article element:', closestArticle);
