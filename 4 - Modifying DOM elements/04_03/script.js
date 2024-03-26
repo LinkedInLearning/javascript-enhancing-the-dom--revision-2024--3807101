14e9fc6521c708cc867de8f10c860df44f51da4f
Maaike <maaikejvp@hotmail.com>
2024-03-26 20:56:40 +0100

    starter lines

--- 4 - Modifying DOM elements/04_03/script.js
+++ 4 - Modifying DOM elements/04_03/script.js
@@ -1,3 +1,5 @@
 function changeAttributes() {
-    
+    // Changing the source of an image
+    let exampleImage = document.getElementById('exampleImage');
+    exampleImage.setAttribute('src', 'puppy2.jpg');
 }
