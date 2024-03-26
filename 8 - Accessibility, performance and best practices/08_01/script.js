3ffcd8f4d7620b30f1a79549e8a95f794ceb81a4
Maaike <maaikejvp@hotmail.com>
2024-03-26 21:12:59 +0100

    updated starter file

--- 8 - Accessibility, performance and best practices/08_01/script.js
+++ 8 - Accessibility, performance and best practices/08_01/script.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const employeeList = document.getElementById('employeeList');
     const employees = ['John Doe', 'Jane Smith', 'Emma Johnson'];
 
-    
+    // appending multiple list items
+    employees.forEach(employee => {
+        let listItem = document.createElement('li');
+        listItem.textContent = employee;
+        document.getElementById('employeeList').appendChild(listItem);
+    });
+
+
 });
