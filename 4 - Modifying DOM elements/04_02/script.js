5b0870d18d91f159803b2c26f78c7febca13552e
Maaike <maaikejvp@hotmail.com>
2024-03-26 18:43:46 +0100

    extra lines updatePage

--- 4 - Modifying DOM elements/04_02/script.js
+++ 4 - Modifying DOM elements/04_02/script.js
@@ -1,3 +1,7 @@
 function updatePage() {
-
+    
+    // Reading and changing the value of a text input
+    let nameInput = document.getElementById('nameInput');
+    console.log('Name:', nameInput.value);
+    nameInput.value = 'John Doe';
 }
